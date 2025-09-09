81968bc822e8d030f25a2e9dd780352fcccb7c92
harveyleungg <harveyleung2010@gmail.com>
2025-09-09 18:41:30 +0800

    Completed assignment

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+        int age = read.nextInt();
+
+        int youngest = 7 + (age / 2);
+        System.out.println(age + "-year olds should date somebody who is at least " + youngest + " years old.");
+
+        read.close();
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
 
+        System.out.println("Enter your age: ");
+        int age = read.nextInt();
+        int pages;
+
+        if (age > 100) {
+            pages = 0;
+        } else {
+            pages = 100 - age;
+        }
+
+        System.out.println(age + "-year olds should read at least " + pages + " pages before giving up on a book.");
+
+        read.close();
     }
 }
