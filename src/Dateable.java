f307439a82b9110ae4a32eef4e78704d86e0ac14
harveyleungg <harveyleung2010@gmail.com>
2025-09-10 11:21:00 +0800

    Added errors and organized code

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,15 +1,30 @@
+// import Scanner for user input
 import java.util.Scanner;
 
+// define Dateable class
 public class Dateable {
+    // main - entry point of program
     public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
+        // create Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
 
+        // prompt user to enter age
         System.out.println("Enter your age: ");
-        int age = read.nextInt();
 
-        int youngest = 7 + (age / 2);
-        System.out.println(age + "-year olds should date somebody who is at least " + youngest + " years old.");
+        // checks if input is a valid integer
+        if (scanner.hasNextInt()) {
+            // stores user input as an integer
+            int age = scanner.nextInt();
+            // calculates minimum dating age for user
+            int youngest = 7 + (age / 2);
 
-        read.close();
+            // outputs user's age and calculated minimum dating age
+            System.out.println(age + "-year olds should date somebody who is at least " + youngest + " years old.");
+        } else {
+            // displays error message for invalid input
+            System.out.println("Please enter a valid age integer");
+        }
+        // closes scanner to prevent resource leaks
+        scanner.close();
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,22 +1,39 @@
+// import Scanner for user input
 import java.util.Scanner;
 
+// define Dateable class
 public class Pages {
-
+    // main - entry point of program
     public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
+        // create Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
 
+        // prompt user to enter age
         System.out.println("Enter your age: ");
-        int age = read.nextInt();
-        int pages;
 
-        if (age > 100) {
-            pages = 0;
+        // checks if input is a valid integer
+        if (scanner.hasNextInt()) {
+            // stores user input as an integer
+            int age = scanner.nextInt();
+            // initiates variable for number of pages
+            int pages;
+            // ensures pages won't end up being negative
+            if (age > 100) {
+                // if age is over 100 then pages will be 0
+                pages = 0;
+            } else {
+                // calculates minimum pages user should read based on age
+                pages = 100 - age;
+            }
+
+            // displays error message for invalid input
+            System.out.println(age + "-year olds should read at least " + pages + " pages before giving up on a book.");
         } else {
-            pages = 100 - age;
-        }
+            // displays error message for invalid input
+            System.out.println("Please enter a valid age integer");
+            }
 
-        System.out.println(age + "-year olds should read at least " + pages + " pages before giving up on a book.");
-
-        read.close();
+        // closes scanner to prevent resource leaks
+        scanner.close();
     }
 }
